6107389ca0a7f3113776e304748e37c1683b0f00
glb <glebserg@mail.ru>
2025-03-24 11:22:19 +0300

    rename

--- src/main/java/ru/atc/smartbudget/SmartBudgetApplication.java
+++ src/main/java/ru/atc/smartbudget/SmartBudgetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartbudgetApplication {
+public class SmartBudgetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartbudgetApplication.class, args);
+		SpringApplication.run(SmartBudgetApplication.class, args);
 	}
 
 }
