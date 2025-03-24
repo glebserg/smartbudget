eaca80768d5e80f84a258fab14fbf4a802c55d62
glb <glebserg@mail.ru>
2025-03-24 15:44:07 +0300

    - clear

--- src/main/java/ru/atc/smartbudget/repository/UserRepository.java
+++ src/main/java/ru/atc/smartbudget/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package ru.atc.smartbudget.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.atc.smartbudget.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/ru/atc/smartbudget/services/UserService.java
+++ src/main/java/ru/atc/smartbudget/services/UserService.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
